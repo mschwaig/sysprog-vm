087e88877eae69c444ac39f316f82ee4ad84e0be
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2022-03-06 15:25:50 +0100

    update nixpkgs, switch from unstable to latest NixOS release

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
